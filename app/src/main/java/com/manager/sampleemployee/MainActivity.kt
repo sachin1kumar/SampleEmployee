6f099dc4f598ecc1896a0a6e5784ddff7be95a54
Sachin <sachinkmr375@gmail.com>
2020-06-28 10:07:15 +0530

    worked for both android and ios

--- app/src/main/java/com/manager/sampleemployee/MainActivity.kt
+++ app/src/main/java/com/manager/sampleemployee/MainActivity.kt
@@ -2,10 +2,14 @@ package com.manager.sampleemployee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.jetbrains.handson.mpp.mobile.createApplicationScreenMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_text).text = createApplicationScreenMessage()
     }
 }
